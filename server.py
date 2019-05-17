2eccdbb52c91a3c93db4461300cd2ee4e5c66498
Trevor Grayson <trevor@trevorgrayson.com>
2019-05-17 06:38:13 +0000

    host __main__

--- server.py
+++ server.py
@@ -5,8 +5,11 @@ from werkzeug.wrappers import Response
 from renderer import render
 
 HOST = os.environ['HOST']
+ENV = os.environ.get('ENV', "PROD")
 
 app = Flask(__name__)
+
+IS_DEV = (ENV == "develop")
 
 @app.route("/")
 def welcome():
@@ -29,5 +32,10 @@ def render_url(path):
     except IOError as err:
         return str(err), 400
 
+<<<<<<< HEAD
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
+=======
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=IS_DEV) 
+>>>>>>> host __main__
