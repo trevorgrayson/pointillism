eb13486e1f1f310d09a5772a43a41f5165cdb548
trevor grayson <trevor@trevorgrayson.com>
2020-04-24 17:05:29 -0700

    package updates

---
+++ point/server/static/precache-manifest.106096d66f47fe91f53c3c80cdd6e458.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bcf1f995b33aa1bb1beac72dbd3d4cb",
+    "url": "/static/index.html"
+  },
+  {
+    "revision": "970f6e95786d347f02a3",
+    "url": "/static/static/css/main.ca4d7160.chunk.css"
+  },
+  {
+    "revision": "4aad707b95566c380079",
+    "url": "/static/static/js/2.e89ea5fe.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/static/js/2.e89ea5fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "970f6e95786d347f02a3",
+    "url": "/static/static/js/main.e3bf3e68.chunk.js"
+  },
+  {
+    "revision": "4608cab3e2120114b1d6",
+    "url": "/static/static/js/runtime-main.8ed9cd89.js"
+  }
+]);
