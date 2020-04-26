a30d5ce0ed392612f7615a2732ecde2339c91b5f
Trevor Grayson <trevor@trevorgrayson.com>
2020-04-26 10:34:38 -0700

    paypal email

--- point/server/paypal.py
+++ point/server/paypal.py
@@ -1,7 +1,6 @@
 import logging
 from flask import Blueprint, redirect, request, make_response
 from point.models import GitHubUser
-<<<<<<< HEAD
 
 LOG = logging.getLogger(__name__)
 
@@ -13,20 +12,21 @@ class PayPalEvent:
         self.create_time = attrs.get('create_time')
         self.resource_type = attrs.get('resource_type')
         self.event_type = attrs.get('event_type')
-        self.amount = attrs.get('resource', {}).get('amount', {}).get('total')
-        self.email = None
-        # self. = attrs.get('resource/amount/currency
+        self.email = attrs.get('resource', {})\
+                          .get('subscriber', {})\
+                          .get('email')
+        self.amount = attrs.get('resource',{}).get('amount', {}).get('total')
 
 paypal_routes = Blueprint('paypal_routes', __name__)
 
 @paypal_routes.route('/paypal/events', methods=["POST"])
 def paypal_event():
     LOG.info(f"Incoming PayPal Event: {request.get_json()}")
-    event = PayPalEvent(**request.get_json())
+    event = PayPalEvent(**request.get_json(force=True))
     user = GitHubUser.find(email=event.email)
     if user:
         user.amount += event.amount
         if GitHubUser.update(user):
-            return '{}', 200
+            return {"message": "OK"}, 200
 
     return '{"message": "unknown error"}', 500
