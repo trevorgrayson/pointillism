ba3c3164087f4b86438bae4a523a072015239987
Trevor Grayson <trevor@trevorgrayson.com>
2020-06-01 00:09:56 -0700

    on server calculator working
    needs testing

--- point/server/render.py
+++ point/server/render.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request
 
 import logging as log
 from json import dumps
+from point.server.base import get_me
 from point.models import GitHubRepo, GitHubUser, GitResource
 from point.clients.gitcontent import GitContent
 from point.renderer import render, cache_control
@@ -20,19 +21,19 @@ def is_allowed(repo, token):
 
 @render_routes.route('/convert', methods=['post'])
 def convert_endpt():
+    me = get_me()
     url = request.json.get('url')
     if not url:
         return 401, '{"message": "include url param"}'
 
-    protocol, _domain, org, project, *rest = url.split('/')
+    protocol, _, _domain, org, project, *rest = url.split('/')
     rest = "/".join(rest)
 
     repo = GitHubRepo.first_repo(org, project)
+    owner = GitHubUser.first(repo.owner)
     creds = None
-    if is_allowed(repo, request.args.get('token')):
-        log.debug(f"Authenticated as {repo.owner}")
-        owner = GitHubUser.first(repo.owner)
-        creds = owner
+    if owner.cn == repo.owner: # TODO repo.can(user)
+        creds = repo.token
 
     return dumps({
        'url': convert(org, project, rest, creds, protocol=protocol)
