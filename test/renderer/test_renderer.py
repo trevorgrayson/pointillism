1b0f756867b3457bc390bde61d908f17cc4c55c3
Trevor Grayson <trevor@trevorgrayson.com>
2020-06-07 23:04:40 -0700

    more testing

--- test/renderer/test_renderer.py
+++ test/renderer/test_renderer.py
@@ -1,4 +1,6 @@
-from point.renderer import url
+from point.renderer import (
+    url, get_mime, cache_control
+)
 
 HOST = 'http://lolcatsho.local'
 
@@ -13,3 +15,10 @@ class TestRenderer:
 
         result = url(HOST, path, **params)
         assert result == 'http://lolcatsho.local/bob/uncle?param1=value1&param2=value2'
+
+    def test_get_mime(self):
+        assert get_mime('svg') == 'svg+xml'
+
+    def test_cache_control(self):
+        headers = cache_control()
+        assert headers['cache-control'] == 'max-age=0 private'
