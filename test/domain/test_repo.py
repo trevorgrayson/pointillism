59511bf7f503d5aa02113a416fc556c38385837f
Trevor Grayson <trevor@trevorgrayson.com>
2020-06-13 23:48:44 -0700

    WIP: test_repo

--- test/domain/test_repo.py
+++ test/domain/test_repo.py
@@ -5,7 +5,9 @@ from point.models.repo import Repo
 class TestRepo:
     @fixture
     def record(self):
-        return {}
+        return {
+
+        }
 
     @fixture
     def repo(self, record):
@@ -13,3 +15,15 @@ class TestRepo:
 
     def test_init(self, repo):
         assert isinstance(repo, Repo)
+
+    # def test_requires_token(self, repo):
+    #     assert not repo.requires_token
+
+    # def test_label(self, repo):
+    #     assert repo.label == '/'
+
+    # def test_as_json(self, repo):
+    #     pass
+    #
+    # def test_has_owner(self, repo):
+    #     pass
