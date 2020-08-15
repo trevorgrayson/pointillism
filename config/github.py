4e4bd799ae0198d657535917b71c9d62e42c8abd
Trevor Grayson <trevor@trevorgrayson.com>
2020-08-15 13:06:37 -0700

    optional github config

--- config/github.py
+++ config/github.py
@@ -8,8 +8,8 @@ GA_ID = environ.get("GA_ID")
 GITHUB_TOKEN = environ.get("GITHUB_TOKEN")
 
 # for app
-GITHUB_CLIENT_ID = environ['GITHUB_CLIENT_ID']
-GITHUB_SECRET = environ['GITHUB_SECRET']
+GITHUB_CLIENT_ID = environ.get('GITHUB_CLIENT_ID')
+GITHUB_SECRET = environ.get('GITHUB_SECRET')
 GITHUB_STATE = str(uuid.uuid4())  # unique for each user
 
 # authenticated users get 100x more requests
